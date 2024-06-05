bbe3ed8c07b680f07c3eb25b2cdc4f2b7167e862
ChangHoon Hahn <changhoon.hahn@princeton.edu>
2024-06-05 18:22:46 +0200

    added .nojekyll file

--- bin/cmass/README.md
+++
@@ -1,8 +0,0 @@
-# SimBIG CMASS
-all the scripts for anlayzing CMASS with SimBIG 
-
-## `fm/`
-includes scripts for forward modeling galaxy mocks, randoms, etc. 
-
-
-constructing galaxy mocks, measuring observables of the CMASS data
--- src/simbig/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = "ChangHoon Hahn"
-__email__ = "changhoon.hahn@princeton.edu"
-__uri__ = "https://github.com/changhoonhahn/boss_sbi"
-__license__ = "MIT"
-__description__ = "Python Package for analyzing SDSS-III with Simulation Based Inference"
-__copyright__ = "Copyright 2020 ChangHoon Hahn"
